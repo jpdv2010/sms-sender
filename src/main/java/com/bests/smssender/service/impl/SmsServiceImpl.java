50b2c897ad69ca13b2da6d65894df890234d0558
jpdv2010 <ims2011>
2019-08-16 10:17:00 -0300

    improvements

--- src/main/java/com/bests/smssender/service/impl/SmsServiceImpl.java
+++ src/main/java/com/bests/smssender/service/impl/SmsServiceImpl.java
@@ -3,21 +3,15 @@ package com.bests.smssender.service.impl;
 import com.bests.smssender.data.dto.SmsData;
 import com.bests.smssender.service.SmsService;
 import com.twilio.Twilio;
-import com.twilio.base.ResourceSet;
-import com.twilio.rest.api.v2010.account.AvailablePhoneNumberCountry;
 import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.rest.api.v2010.account.availablephonenumbercountry.Mobile;
 import com.twilio.type.PhoneNumber;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -26,8 +20,9 @@ public class SmsServiceImpl implements SmsService {
     private ArrayList<Long> SMS_SUCCESS_STATUS_CODE = new ArrayList<Long>( Arrays.asList(200L,201L));
     private String SMS_API_URL = "http://api.smsempresa.com.br/send";
     private String KEY = "745T4QGBQM3GU6BYJ8ZBXQ7R";
-    private static final String ACCOUNT_SID = "AC44cba8b0f1daad15e7fcfd278c83806f";
-    private static final String AUTH_TOKEN = "2dbe68a5177c48ba5b4181e6a99ccb26";
+    private static final String TWILIO_ACCOUNT_SID = "AC44cba8b0f1daad15e7fcfd278c83806f";
+    private static final String TWILIO_AUTH_TOKEN = "2dbe68a5177c48ba5b4181e6a99ccb26";
+    private static final String TWILIO_NUMBER = "+15005550006";
 
     @Override
     public void sendSms(String message, String number) throws Exception {
@@ -56,12 +51,12 @@ public class SmsServiceImpl implements SmsService {
 
     @Override
     public void sendTwilio(String message, String number){
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Twilio.init( TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN );
 
-        Message msg = Message.creator(new PhoneNumber(number),
-                new PhoneNumber("+15005550006"),//"+553140421656"),
-                message).create();
+        Message msg = Message.creator( new PhoneNumber( number ),
+                new PhoneNumber( TWILIO_NUMBER ),
+                message ).create();
 
-        System.out.println(msg.getSid());
+        System.out.println( msg.getSid() );
     }
 }
